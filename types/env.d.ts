73cbf96e487d504ca69f3b5e35561821af73b984
Андрей Епифанов <epifanovmd@gmail.com>
2024-08-31 00:49:48 +0300

    refactoring

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,12 +1,20 @@
 interface EnvVariables {
+  BASE_URL: string;
+  MODE: string;
+  DEV: boolean;
+  PROD: boolean;
+  SSR: boolean;
+
   VITE_DEV_PROTOCOL: string;
   VITE_DEV_HOST: string;
   VITE_DEV_PORT: string;
   VITE_BASE_URL: string;
+  VITE_SOCKET_BASE_URL: string;
+
   VITE_APP_NAME: string;
 }
 
-type ImportMetaEnv = EnvVariables
+type ImportMetaEnv = EnvVariables;
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
