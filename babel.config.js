333ab0813e4aa54850000fb856b9b924a2b58b27
epifanovmd <epifanovmd@gmail.com>
2026-07-12 00:19:26 +0300

    refactoring

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ export default {
   presets: [],
   plugins: [
     ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: false }],
     "babel-plugin-transform-typescript-metadata",
     "babel-plugin-parameter-decorator",
   ],
